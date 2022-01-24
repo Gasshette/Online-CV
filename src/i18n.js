b76429a93a2ebb255f4b80b1b4b7e7707bc7e33e
Gasshette <gesshokun@gmail.com>
2022-01-24 20:34:25 +0100

    feat(upgrade): upgrade packages
    upgrade react and all the packages used in the app

--- src/i18n.js
+++ src/i18n.js
@@ -4,14 +4,17 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import moment from 'moment';
 
+import 'moment/locale/fr';
+import 'moment/locale/en-gb';
+
 const fallbackLng = ['fr'];
 const availableLanguages = ['fr', 'en'];
 
-availableLanguages.forEach((element) => {
-  if (element !== 'en') {
-    import(`moment/locale/${element}`);
-  }
-});
+// availableLanguages.forEach((element) => {
+//   if (element !== 'en') {
+//     import(`moment/locale/${element}`);
+//   }
+// });
 
 i18n
   .use(Backend)
